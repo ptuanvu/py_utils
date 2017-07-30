8d3b7e281a2c3048ba62a5d11a14be910e7e3a02
Vu Tran <monster@Monsters-MacBook-Air.local>
2017-07-31 01:04:01 +0700

    Cho tot filter

--- chotot.py
+++ chotot.py
@@ -1,19 +1,51 @@
-def ask_ok(prompt, retries=4, reminder='Please try again!'):
-    while True:
-        ok = input(prompt)
-        if ok in ('y', 'ye', 'yes'):
-            return True
-        if ok in ('n', 'no', 'nop', 'nope'):
-            return False
-        retries = retries - 1
-        if retries < 0:
-            raise ValueError('invalid user response')
-        print(reminder)
+import requests
+import json
 
-i = 5
+LOW = 50000000
+HIGH = 90000000
+isFree = 1
+def findBike( url ):
+  # print "FIND BIKE: " + url
+  r = requests.get(url)
 
-def f(arg=i):
-    print(arg)
+  if r.status_code == 200:
+    #  print "REQUEST SUCCESSFUL"
+     respone_native = json.loads(r.text)
+     for ads in respone_native["ads"]:
+      if ads.has_key("price") and ads["price"] > LOW and ads["price"] < HIGH:
+        print "___TITLE: " + ads["subject"].encode('utf-8')
+        print "___PRICE: " + ads["price_string"].encode('utf-8')
+        print "___DATE: " + ads["date"].encode('utf-8')
+        print "______________________________________________________________"
+      isFree = 1
+  else:
+     print "REQUEST FAILED"
+  return
 
-i = 6
-f()
+# BASE_URL = "https://gateway.chotot.com/v1/public/ad-listing?region=13&cg=2020&w=1&sp=0&limit=20&o=0&st=s,k"
+BASE_URL = "https://gateway.chotot.com/v1/public/ad-listing"
+LIMIT = 50
+
+f_type = "p" # p -> personal, c -> company
+region = "13" # TP HCM
+cg = "2020" # Xe may
+sp = "0" # filter
+
+BASE_URL += "?region=" + region
+BASE_URL += "&cg=" + cg
+BASE_URL += "&w=1"
+BASE_URL += "&f=" + f_type
+BASE_URL += "&sp=0"
+BASE_URL += "&limit=20"
+
+count = 0
+while (count < LIMIT):
+   if 1:
+     url = BASE_URL + "&o=" + str(count * 20) + "&st=s,k"
+     findBike(url)
+     isFree = 0
+     count = count + 1
+   else:
+     print "HOLDING"
+
+print "Good bye!"
